a4eb031aa2acc0681a557f3b62d4f588e5638d7f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-10 12:41:02 -0300

    Validacao de expansao

--- cypress/integration/square.spec.js
+++ cypress/integration/square.spec.js
@@ -1,9 +1,29 @@
+const pointerEvent = {
+    force: true,
+    pointerType: 'touch'
+}
+
 describe('Teste Expansão Quadrado', () => {
+    
     it('Quadrado renderizado', () => {
         cy.visit("/")
 
         cy.get(".square")
             .invoke('outerWidth')
             .should('be.eq', 90)
+    });
+
+    it('Quadrado Clicável', () => {
+        cy.get(".square")
+            .trigger('pointerdown', pointerEvent)
+
+        cy.wait(500)
+
+        cy.get(".square")
+            .trigger('pointerup', pointerEvent)
+
+        cy.get(".square")
+            .invoke('outerWidth')
+            .should('be.eq', 225)
     });
 });
